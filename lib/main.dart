85994e39d0377c723bbc9b79567ed31db51ef533
Yakiv Mospan <yakiv.mospan@gmail.com>
2020-05-09 01:30:08 +0300

    Added tab navigation and dummy routes for it.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 
-import 'presentation/home_screen.dart';
+import 'presentation/home_route.dart';
 import 'theme.dart';
 
 void main() => runApp(CoffeeShop());
 
 class CoffeeShop extends StatelessWidget {
+  const CoffeeShop();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "CoffeeShop",
       theme: mainTheme,
-      home: HomeScreen(),
+      home: HomeRoute(),
     );
   }
 }
---
+++ lib/presentation/home_route.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/presentation/basket_route.dart';
+import 'package:flutterapp/presentation/location_route.dart';
+import 'package:flutterapp/presentation/orders_route.dart';
+import 'package:flutterapp/presentation/profile_route.dart';
+import 'package:flutterapp/presentation/shop_route.dart';
+
+class HomeRoute extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeRouteState();
+  }
+}
+
+class _HomeRouteState extends State<HomeRoute> {
+  int _selectedTabIndex = 0;
+
+  void _onTabSelected(int newIndex) {
+    setState(() {
+      _selectedTabIndex = newIndex;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _TabView(selectedTabIndex: _selectedTabIndex),
+      bottomNavigationBar: _BottomTabsView(
+        selectedTabIndex: _selectedTabIndex,
+        onTapListener: _onTabSelected,
+      ),
+    );
+  }
+}
+
+class _TabView extends StatelessWidget {
+  final int selectedTabIndex;
+
+  _TabView({
+    @required this.selectedTabIndex,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (selectedTabIndex == locationTabIndex) {
+      return LocationRoute();
+    } else if (selectedTabIndex == shopTabIndex) {
+      return ShopRoute();
+    } else if (selectedTabIndex == ordersTabIndex) {
+      return OrdersRoute();
+    } else if (selectedTabIndex == basketTabIndex) {
+      return BasketRoute();
+    } else if (selectedTabIndex == profileTabIndex) {
+      return ProfileRoute();
+    } else {
+      throw ("Illegal argument exception: invalid selectedTabIndex.");
+    }
+  }
+}
+
+class _BottomTabsView extends StatelessWidget {
+  final int selectedTabIndex;
+  final ValueChanged<int> onTapListener;
+
+  _BottomTabsView({
+    @required this.selectedTabIndex,
+    @required this.onTapListener,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO add StringsProvider for multi-lang support
+    // TODO add correct icons
+
+    return BottomNavigationBar(
+      unselectedItemColor: Colors.black,
+      selectedItemColor: Colors.teal[400],
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.location_on),
+          title: Text('Location'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.shop),
+          title: Text('Shop'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.note),
+          title: Text('Orders'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.shopping_cart),
+          title: Text('Basket'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.account_circle),
+          title: Text('Profile'),
+        ),
+      ],
+      currentIndex: selectedTabIndex,
+      onTap: onTapListener,
+    );
+  }
+}
+
+const int locationTabIndex = 0;
+const int shopTabIndex = 1;
+const int ordersTabIndex = 2;
+const int basketTabIndex = 3;
+const int profileTabIndex = 4;
