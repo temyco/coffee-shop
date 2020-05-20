a57f93ff2e191c9af56889e2d0a2aea484d9b39d
Volodymyr Yatsykiv <volodymyryatsykiv@gmail.com>
2020-05-20 19:02:01 +0300

    Added AppTextWidget.

---
+++ lib/widgets/text/app_text_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTextWidget extends Text {
+  AppTextWidget(
+    String data,
+    TextStyle style,
+  ) : super(
+          data,
+          style: style,
+        );
+}
